510146fe6f010ae735a48c40a7c9e2b9dc921aeb
E33EPUS <@example.com>
2026-08-15 21:08:35 +0800

    refactor: remove dead code (drawPlayerHead, drawTextureIcon, computeTotalH, ChatLayout.of/effectiveMsgBottom, unused vars)

--- src/main/java/com/niuqu/chatbubble/render/ChatLayout.java
+++ src/main/java/com/niuqu/chatbubble/render/ChatLayout.java
@@ -9,16 +9,4 @@ public record ChatLayout(int panelX, int panelW, int titleY, int msgTop, int msg
     static final int NOTIF_H = 14;
     public static final int SIDEBAR_W = 90;
 
-    public static ChatLayout of(int screenW, int screenH, int panelWidthConfig, int guiScale,
-                                boolean sidebarOpen) {
-        int pw = Math.max(100, Math.min(panelWidthConfig / guiScale, screenW));
-        int px = sidebarOpen ? SIDEBAR_W : 0;
-        if (px + pw > screenW) pw = screenW - px;
-        int bt = screenH - BAR_H;
-        return new ChatLayout(px, pw, 0, TITLE_H + 1, bt - 1, bt, screenW, screenH);
-    }
-
-    public int effectiveMsgBottom(boolean hasNewMessages) {
-        return hasNewMessages ? barTop - NOTIF_H - 1 : msgBottom;
-    }
 }
